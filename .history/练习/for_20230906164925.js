4f91e3d6748c7fd82c5f4a9c730cbdd3250bac3f
wgqiu <wgqiu@sse.com.cn>
2023-09-06 16:57:46 +0800

    javascript

---
+++ .history/练习/for_20230906164925.js
@@ -0,0 +1,6 @@
+let i = 3
+let array = new array[10]
+for (let index = 0; index < array.length; index++) {
+    i = i + index
+    
+}
---
+++ .history/练习/for_20230906165318.js
@@ -0,0 +1,5 @@
+let i = 3
+for (let index = 0; index < 4; index++) {
+    i = i + index
+    
+}
